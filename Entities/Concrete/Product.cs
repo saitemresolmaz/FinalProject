4c0da0b06a287804aef1c17f1dd44580e04e6edb
saitemresolmaz <76459268+saitemresolmaz@users.noreply.github.com>
2021-02-05 23:47:55 +0300

    Add project files.

---
+++ Entities/Concrete/Product.cs
@@ -0,0 +1,22 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    //bir classın default hali internal'dır. internal sadece entities erişebilir demek.
+    public class Product : IEntity
+    //public yaptık diğer katmanlar bu class'a erişebilsin diye.
+    {
+        public int ProductId { get; set; }
+        public int CategoryId { get; set; }
+        public string ProductName { get; set; }
+
+        public short UnitsInStock { get; set; } //short int'in küçüğü.
+
+        public decimal UnitPrice { get; set; }  //decimal para birimi. 
+
+
+    }
+}
