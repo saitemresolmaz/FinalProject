596ff696ea70728f992989c04212718cb3993d1e
saitemresolmaz <76459268+saitemresolmaz@users.noreply.github.com>
2021-02-14 15:59:44 +0300

    Core Layer

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -11,17 +11,39 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            //DTO > Data Transformation Object
+            ProductTest();
+            //CategoryTest();
 
+        }
+
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
             ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var product in productManager.GetAllByCategoryId(2)) //2 numaralı kategorideki ürünler
-            {
-                Console.WriteLine(product.ProductName);
-            }
+            //foreach (var product in productManager.GetAllByCategoryId(2)) //2 numaralı kategorideki ürünler
+            //{
+            //    Console.WriteLine(product.ProductName);
+            //}
 
-            foreach (var product2 in productManager.GetByUnitPrice(40, 100)) //fiyatı 40 ile 100 arasında olanlar
+            //foreach (var product2 in productManager.GetByUnitPrice(40, 100)) //fiyatı 40 ile 100 arasında olanlar
+            //{
+            //    Console.WriteLine(product2.UnitPrice + " " + product2.ProductName);
+            //}
+
+            foreach (var product in productManager.GetProductDetails()) //2 numaralı kategorideki ürünler
             {
-                Console.WriteLine(product2.UnitPrice + " " + product2.ProductName);
+                Console.WriteLine(product.ProductName + "/" + product.CategoryName);
             }
         }
     }
--- Core/DataAccess/IEntityRepository.cs
+++ Core/DataAccess/IEntityRepository.cs
@@ -1,10 +1,11 @@
-﻿using Entities.Abstract;
+﻿using Core.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Text;
 
-namespace DataAccess.Abstract
+// core katmanı diğer katmanları referans almaz.
+namespace Core.DataAccess
 {
     //generic constraint > kısıtlama
     //class: referans tip
