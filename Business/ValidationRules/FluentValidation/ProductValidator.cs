6589291b8470dc1429124a5b4c0b8649c2f0640a
saitemresolmaz <76459268+saitemresolmaz@users.noreply.github.com>
2021-02-25 18:34:18 +0300

    Aspect Validation yapma

---
+++ Business/ValidationRules/FluentValidation/ProductValidator.cs
@@ -0,0 +1,31 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class ProductValidator : AbstractValidator<Product>
+    {
+        public ProductValidator()
+        {
+            //p Product'a karşılık gelir.
+
+            RuleFor(p => p.ProductName).NotEmpty();
+            RuleFor(p => p.ProductName).MinimumLength(2);
+            RuleFor(p => p.UnitPrice).NotEmpty();
+            RuleFor(p => p.UnitPrice).GreaterThan(0);
+            RuleFor(p => p.UnitPrice).GreaterThan(10).When(p => p.CategoryId == 1); // Beverages categori Id'si 1
+
+            RuleFor(p => p.ProductName).Must(StartWithA).WithMessage("Ürünler A harfi ile başlamalı"); //kendi kuralımızı yazıyoruz.
+
+
+        }
+
+        private bool StartWithA(string arg)
+        {
+            return arg.StartsWith("A"); // A ile başlarsa true döner. Başlamıyorsa false döner.
+        }
+    }
+}
--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -32,8 +32,8 @@ namespace WebAPI
             //Autofac, Ninject, CastleWindsor,StructureMap, LightInject, DryInject --> IoC Container
             //Autofac kullanacaðýz çünkü AOP tekniðini kullanýyor.
             services.AddControllers();
-            services.AddSingleton<IProductService,ProductManager>(); //birisi IProductService isterse arkaplanda ona new'lenmiþ ProductManager ver.
-            services.AddSingleton<IProductDal, EfProductDal>();
+            //services.AddSingleton<IProductService,ProductManager>(); //birisi IProductService isterse arkaplanda ona new'lenmiþ ProductManager ver.
+            //services.AddSingleton<IProductDal, EfProductDal>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
