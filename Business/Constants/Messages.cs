63f7f4fed2f6048a64fcb98a85c66baeb0fefd25
saitemresolmaz <76459268+saitemresolmaz@users.noreply.github.com>
2021-03-03 01:39:40 +0300

    Security İşlemleri Authorization

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Core.Entities.Concrete;
+using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace Business.Constants
@@ -13,5 +15,12 @@ namespace Business.Constants
         public static string ProductCountOfCategoryError = "Kategorideki ürün limitini aştınız";
         public static string ProductNameAlreadyExists = "Aynı isimde ürün var";
         public static string CategoryLimitExceded = "Kategori limiti aşıldığı için yeni bir ürün eklenemiyor";
+        public static string AuthorizationDenied = "Yetkiniz yok";
+        public static string UserRegistered = "Kayıt oldu";
+        public static string UserNotFound = "Kullanıcı bulunamadı";
+        public static string PasswordError = "Parola hatası";
+        public static string SuccessfulLogin = "Başarılı giriş";
+        public static string UserAlreadyExists = "Kullanıcı mevcut";
+        internal static string AccessTokenCreated =" Token oluşturuldu";
     }
 }
